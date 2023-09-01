f4ccbf73068043e3516cc707936232b5133f2c1b
Test User <ahnaf.shakil24@gmail.com>
2023-09-01 22:58:25 +0600

    de

--- index.js
+++ index.js
@@ -5,16 +5,28 @@ canvas.width = 1024
 canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
+const gravity = 0.2
 
 class Sprite {
     constructor({position, velocity}) {
         this.position = position
         this.velocity = this.velocity
+        this.height = 150
     }
 
     draw() {
         c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, 50, 150)
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+    
+    update() {
+        this.draw()
+       
+        this.position.y += this.velocity.y
+
+        if(this.position + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        }else  this.gravity.y += gravity
     }
 }
 
@@ -29,7 +41,7 @@ const player = new Sprite({
  }
 })
 
-player.draw()
+
 
 const enemy = new Sprite({
     position: {
@@ -42,13 +54,25 @@ const enemy = new Sprite({
  }
 })
 
-enemy.draw()
+
 
 console.log(player)
 
 function animate() {
     window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
     console.log('go')
+    player.update()
+    enemy.update()
 }
 
 animate()
+
+window.addEventListener('keydown', (event) => {
+    switch(event.key) {
+        case 'd':
+            break
+    }
+    console.log(event.key)
+})
